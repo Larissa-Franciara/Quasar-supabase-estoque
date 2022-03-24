f3ad84d5a1526e295e290dd9675267c4d4f7a9a3
larissa-franciara <larissafranciaradealmeida@gmail.com>
2022-03-24 12:40:52 -0300

    Projeto de estoque quasar/supabase

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+
+          <q-avatar size="50px">
+            <img src="https://images.vexels.com/media/users/3/130943/isolated/lists/6ce6689b20ca2489fed316da0c187450-etiqueta-de-diamante-moderno.png">
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, onMounted} from 'vue'
+import useApi from "src/composables/UseApi";
+
+
+export default defineComponent({
+  name: 'LoginLayout',
+
+
+  setup () {
+
+    const {getBrand} = useApi()
+
+    onMounted(() => {
+      getBrand()
+    })
+
+    return {
+
+      }
+    }
+})
+</script>
---
+++ src/pages/product/Public.vue
@@ -0,0 +1,204 @@
+<template>
+  <q-page padding>
+    <div class="row" v-if="brand.name" >
+      <div class="col-12 text-center text-h4">
+        {{brand.name}}
+      </div>
+    </div>
+
+    <div class="row" >
+      <q-select
+        outlined v-model="categoryId"
+        :options="optionsCategories"
+        label="Category"
+        option-label="name"
+        option-value="id"
+        map-options
+        emit-value
+        clearable
+        class="col-4 q-mt-sm"
+        dense
+        @update:model-value="handleListProducts(route.params.id)"
+      />
+
+      <q-table
+        :rows="products"
+        :columns="columnsProduct"
+        v-model:pagination="initialPagination"
+        row-key="id"
+        class="col-12"
+        :loading="loading"
+        :filter="filter"
+        grid
+        hide-pagination
+      >
+
+        <template v-slot:top>
+          <span class="text-h6">
+            Products
+          </span>
+          <q-space />
+
+          <q-input
+            outline
+            dense
+            debounce="300"
+            v-model="filter"
+            placeholder="Search"
+            class="q-mr-sm">
+
+            <template v-slot:append>
+              <q-icon
+                name="search"
+                color="dark"
+              />
+            </template>
+          </q-input>
+        </template>
+
+        <template v-slot:item="props">
+          <div class="q-pa-xs col-xs-12 col-sm-6 col-md-3">
+            <q-card class="cursor-pointer" v-ripple:primary @click="handleShowDetails(props.row)">
+              <q-img :src="props.row.img_url" :ratio="4/3" />
+              <q-card-section class="text-center">
+                <div class="text-h6">{{ props.row.name }}</div>
+                <div class="text-subtitle2">{{ formatCurrency(props.row.price)}}</div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <div class="col-12" v-if="props.rowIndex === 3 && brand.paralax_url">
+            <q-parallax :height="225" :speed="0.5" >
+              <template v-slot:media >
+                <img :src="brand.paralax_url" style="height: 300px">
+              </template>
+            </q-parallax>
+          </div>
+
+        </template>
+      </q-table>
+    </div>
+
+    <div class="row justify-center">
+      <q-pagination
+        class="q-mt-sm"
+        v-model="initialPagination.page"
+        :max="pagesNumber"
+        direction-links
+        @update:model-value="handleScrollToTop"
+
+      />
+    </div>
+
+<dialog-product-details
+  :show="showDialogDetails"
+  :product="productDetails"
+  @hide-dialog="showDialogDetails = false"
+ />
+
+  </q-page>
+</template>
+
+<script>
+
+
+import {defineComponent, ref, onMounted, computed} from 'vue';
+import useApi from "src/composables/UseApi";
+import useNotify from "src/composables/UseNotify";
+import {useRoute} from 'vue-router';
+import {columnsProduct, initialPagination} from './table'
+import {formatCurrency} from "src/utils/format";
+import DialogProductDetails from "components/DialogProductDetails";
+
+
+export default defineComponent({
+  name: "PageProductPublic",
+  components: {
+    DialogProductDetails
+  },
+
+  setup () {
+    const products = ref([])
+
+    const loading = ref(true)
+
+    const filter = ref('')
+
+    const table = 'product'
+
+    const showDialogDetails = ref(false)
+
+    const productDetails = ref({})
+
+    const optionsCategories = ref([])
+
+    const categoryId = ref('')
+
+    const { listPublic, brand} = useApi()
+
+
+    const {notifyError} = useNotify()
+
+    const route = useRoute()
+
+    const handleListProducts = async (userId) => {
+      try {
+        loading.value = true
+        products.value = categoryId.value ? await listPublic(table, userId, 'category_id', categoryId.value) : await listPublic(table, userId)
+        loading.value = false
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    const handleShowDetails = (product) => {
+      productDetails.value = product
+      showDialogDetails.value = true
+    }
+
+    const handleListCategories = async (userId) => {
+      optionsCategories.value = await listPublic('category', userId)
+
+    }
+
+    const handleScrollToTop = () => {
+      window.scrollTo({top: 0, behavior: 'smooth' })
+    }
+
+    onMounted (() => {
+      if(route.params.id) {
+        handleListCategories(route.params.id)
+        handleListProducts(route.params.id)
+      }
+    })
+
+
+    return {
+      columnsProduct,
+      products,
+      loading,
+      filter,
+      formatCurrency,
+      showDialogDetails,
+      productDetails,
+      handleShowDetails,
+      handleListProducts,
+      handleScrollToTop,
+      brand,
+      optionsCategories,
+      categoryId,
+      route,
+      initialPagination,
+      pagesNumber: computed(() => Math.ceil(products.value.length / initialPagination.value.rowsPerPage))
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
+
+
+pixomatic
